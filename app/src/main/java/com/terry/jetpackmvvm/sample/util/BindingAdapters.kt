ba193da62326cf60fded1921c1d5a58487c37659
Terry Li <agoodboy30225@gmail.com>
2021-08-08 16:40:53 +0800

    fix(glide): failed to find GeneratedAppGlideModule

--- app/src/main/java/com/terry/jetpackmvvm/sample/util/BindingAdapters.kt
+++ app/src/main/java/com/terry/jetpackmvvm/sample/util/BindingAdapters.kt
@@ -2,13 +2,13 @@ package com.terry.jetpackmvvm.sample.util
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import com.bumptech.glide.Glide
+import com.terry.jetpackmvvm.sample.GlideApp
 
 object BindingAdapters {
     @JvmStatic
     @BindingAdapter("imageUrl")
     fun bindImage(imageView: ImageView, url: String) {
-        Glide.with(imageView.context)
+        GlideApp.with(imageView.context)
             .load(url)
             .into(imageView)
     }
