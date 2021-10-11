46dbfdce9d0621bedd9a9f1632fe4b3dcd40735a
Terry Li <agoodboy30225@gmail.com>
2021-10-11 16:00:32 +0800

    feat: add rxbinding sample

--- app/src/main/java/com/terry/jetpackmvvm/sample/ui/utiltest/UtilTestFragment.kt
+++ app/src/main/java/com/terry/jetpackmvvm/sample/ui/utiltest/UtilTestFragment.kt
@@ -1,10 +1,10 @@
 package com.terry.jetpackmvvm.sample.ui.utiltest
 
 import androidx.lifecycle.ViewModelProvider
-import android.os.Bundle
 import com.terry.jetpackmvvm.sample.R
 import com.terry.jetpackmvvm.sample.databinding.FragmentUtilTestBinding
 import com.terry.jetpackmvvm.sample.ui.base.BaseFragment
+import com.terry.jetpackmvvm.sample.util.RxUtils
 import com.terry.jetpackmvvm.sample.widget.AnimDialog
 
 class UtilTestFragment : BaseFragment<FragmentUtilTestBinding>() {
@@ -15,10 +15,21 @@ class UtilTestFragment : BaseFragment<FragmentUtilTestBinding>() {
     override fun init() {
         viewModel = ViewModelProvider(this).get(UtilTestViewModel::class.java)
 
-        binding.btnScreenshot.setOnClickListener { UtilTestUtils.showScreenShotDialog(parentFragmentManager) }
-        binding.btnAnimDialogZoom.setOnClickListener { UtilTestUtils.showAnimDialog(requireContext(), AnimDialog.AnimInType.CENTER_ZOOM) }
-        binding.btnAnimDialogTop.setOnClickListener { UtilTestUtils.showAnimDialog(requireContext(), AnimDialog.AnimInType.TOP) }
-        binding.btnAnimDialogBottom.setOnClickListener { UtilTestUtils.showAnimDialog(requireContext(), AnimDialog.AnimInType.BOTTOM) }
+        RxUtils.throttleFirst(viewLifecycleOwner, binding.btnScreenshot, {
+            UtilTestUtils.showScreenShotDialog(parentFragmentManager)
+        })
+
+        RxUtils.throttleFirst(viewLifecycleOwner, binding.btnAnimDialogZoom, {
+            UtilTestUtils.showAnimDialog(requireContext(), AnimDialog.AnimInType.CENTER_ZOOM)
+        })
+
+        RxUtils.throttleFirst(viewLifecycleOwner, binding.btnAnimDialogTop, {
+            UtilTestUtils.showAnimDialog(requireContext(), AnimDialog.AnimInType.TOP)
+        })
+
+        RxUtils.throttleFirst(viewLifecycleOwner, binding.btnAnimDialogBottom, {
+            UtilTestUtils.showAnimDialog(requireContext(), AnimDialog.AnimInType.BOTTOM)
+        })
     }
 
     companion object {
