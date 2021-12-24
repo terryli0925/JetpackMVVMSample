7de40ee4dfebe6fe796b96628abee63510781d83
Terry <Terry@iter.com.tw>
2021-12-24 18:19:22 +0800

    v1

--- app/src/main/java/com/terry/jetpackmvvm/sample/ui/SecondActivity.kt
+++ app/src/main/java/com/terry/jetpackmvvm/sample/ui/SecondActivity.kt
@@ -1,27 +1,20 @@
 package com.terry.jetpackmvvm.sample.ui
 
-import android.content.pm.ActivityInfo
-import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.shuyu.gsyvideoplayer.GSYVideoManager
+import com.shuyu.gsyvideoplayer.listener.GSYSampleCallBack
 import com.shuyu.gsyvideoplayer.utils.OrientationUtils
-import com.superluo.textbannerlibrary.ITextBannerItemClickListener
-import com.superluo.textbannerlibrary.TextBannerView
+import com.shuyu.gsyvideoplayer.video.base.GSYVideoPlayer
 import com.terry.jetpackmvvm.sample.R
 import com.terry.jetpackmvvm.sample.widget.MarqueeVideoPlayer
-import com.shuyu.gsyvideoplayer.listener.GSYSampleCallBack
-
-
-
 
 
 class SecondActivity : AppCompatActivity() {
     private lateinit var videoPlayer: MarqueeVideoPlayer
-    private lateinit var tvBanner: TextBannerView
     private lateinit var orientationUtils: OrientationUtils
 
     private var list: ArrayList<String>? = null
@@ -35,81 +28,46 @@ class SecondActivity : AppCompatActivity() {
         val source1 = "http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f20.mp4"
         videoPlayer.backButton.visibility = View.VISIBLE
         //设置旋转
-        //设置旋转
         orientationUtils = OrientationUtils(this, videoPlayer)
-        orientationUtils.isEnable = true
+//        orientationUtils.isEnable = true
+
+
         videoPlayer.setUp(source1, true, "测试视频")
         videoPlayer.fullscreenButton.setOnClickListener { // ------- ！！！如果不需要旋转屏幕，可以不调用！！！-------
-            // 不需要屏幕旋转，还需要设置 setNeedOrientationUtils(false)
-            orientationUtils.resolveByClick()
-            val gsyBaseVideoPlayer = videoPlayer.startWindowFullscreen(this, true, true) as MarqueeVideoPlayer
-            initBanner(gsyBaseVideoPlayer.textBannerView)
-//            finish()
+            videoPlayer.startWindowFullscreen(this, true, true) as MarqueeVideoPlayer
         }
         videoPlayer.setVideoAllCallBack(object : GSYSampleCallBack() {
             override fun onPrepared(url: String, vararg objects: Any) {
                 super.onPrepared(url, *objects)
                 //开始播放了才能旋转和全屏
-                orientationUtils.isEnable = videoPlayer.isRotateWithSystem()
+                orientationUtils.isEnable = true
             }
 
-            override fun onAutoComplete(url: String, vararg objects: Any) {
-                super.onAutoComplete(url, *objects)
-            }
-
-            override fun onClickStartError(url: String, vararg objects: Any) {
-                super.onClickStartError(url, *objects)
+            override fun onEnterFullscreen(url: String?, vararg objects: Any?) {
+                super.onEnterFullscreen(url, *objects)
+                if (objects[1] is MarqueeVideoPlayer) {
+                    val videoView = objects[1] as MarqueeVideoPlayer
+                    setUpMarquee(videoView)
+                }
             }
 
             override fun onQuitFullscreen(url: String, vararg objects: Any) {
                 super.onQuitFullscreen(url, *objects)
 
-                // ------- ！！！如果不需要旋转屏幕，可以不调用！！！-------
-                // 不需要屏幕旋转，还需要设置 setNeedOrientationUtils(false)
-                if (orientationUtils != null) {
-                    orientationUtils.backToProtVideo()
-                }
+//                if (objects[1] is MarqueeVideoPlayer) {
+//                    val videoView = objects[1] as MarqueeVideoPlayer
+//                    setUpMarquee(videoView)
+//                }
             }
         })
-        videoPlayer.backButton.setOnClickListener { onBackPressed() }
-        videoPlayer.startPlayLogic()
 
-        val tvBanner = videoPlayer.textBannerView
-        initBanner(tvBanner)
-//        list = ArrayList()
-//        list?.add("学好Java、Android、C#、C、ios、html+css+js")
-//        list?.add("走遍天下都不怕！！！！！")
-//        list?.add("不是我吹，就怕你做不到，哈哈")
-//        list?.add("superluo")
-//        list?.add("你是最棒的，奔跑吧孩子！")
-//        tvBanner.setDatas(list)
-//        tvBanner.setItemOnClickListener(ITextBannerItemClickListener { data, position ->
-//            Log.i("xxx", "SecondActivity ItemOnClickListener")
-//
-//            Toast.makeText(this,
-//                "$position>>$data",
-//                Toast.LENGTH_SHORT
-//            ).show()
-//        })
-        videoPlayer.tvTest.text = "fsfsdf"
-//        tvBanner = findViewById(R.id.tv_banner)
-//        list = ArrayList()
-//        list?.add("学好Java、Android、C#、C、ios、html+css+js")
-//        list?.add("走遍天下都不怕！！！！！")
-//        list?.add("不是我吹，就怕你做不到，哈哈")
-//        list?.add("superluo")
-//        list?.add("你是最棒的，奔跑吧孩子！")
-//        tvBanner.setDatas(list)
-//
-//        tvBanner.setItemOnClickListener(ITextBannerItemClickListener { data, position ->
-//            Toast.makeText(this,
-//                "$position>>$data",
-//                Toast.LENGTH_SHORT
-//            ).show()
-//        })
+        setUpMarquee(getCurPlay() as MarqueeVideoPlayer)
+
+        videoPlayer.startPlayLogic()
     }
 
-    fun initBanner(tvBanner: TextBannerView) {
+    private fun setUpMarquee(videoView: MarqueeVideoPlayer) {
+        val tvBanner = videoView.textBannerView
         list = ArrayList()
         list?.add("学好Java、Android、C#、C、ios、html+css+js")
         list?.add("走遍天下都不怕！！！！！")
@@ -117,53 +75,43 @@ class SecondActivity : AppCompatActivity() {
         list?.add("superluo")
         list?.add("你是最棒的，奔跑吧孩子！")
         tvBanner.setDatas(list)
-        tvBanner.setItemOnClickListener(ITextBannerItemClickListener { data, position ->
+        tvBanner.setItemOnClickListener { data, position ->
             Log.i("xxx", "SecondActivity ItemOnClickListener")
 
-            Toast.makeText(this,
+            Toast.makeText(
+                this,
                 "$position>>$data",
                 Toast.LENGTH_SHORT
             ).show()
-        })
+        }
         tvBanner.startViewAnimator();
-    }
-
-//    override fun onConfigurationChanged(newConfig: Configuration) {
-//        super.onConfigurationChanged(newConfig)
-//        videoPlayer.onConfigurationChanged(
-//                this,
-//                newConfig,
-//                orientationUtils,
-//                true,
-//                true
-//            )
-//    }
-
-
-    override fun onPause() {
-        super.onPause()
-        videoPlayer.currentPlayer.onVideoPause()
     }
 
     override fun onResume() {
         super.onResume()
         videoPlayer.onVideoResume()
+        val videoView = getCurPlay() as MarqueeVideoPlayer
+        videoView.textBannerView.startViewAnimator()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        videoPlayer.currentPlayer.onVideoPause()
+        val videoView = getCurPlay() as MarqueeVideoPlayer
+        videoView.textBannerView.stopViewAnimator()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         GSYVideoManager.releaseAllVideos()
-        if (orientationUtils != null) orientationUtils.releaseListener()
+        orientationUtils.releaseListener()
     }
 
-    override fun onBackPressed() {
-///       不需要回归竖屏
-        if (orientationUtils.getScreenType() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
-            videoPlayer.getFullscreenButton().performClick();
-            return
+    private fun getCurPlay(): GSYVideoPlayer {
+        return if (videoPlayer.fullWindowPlayer != null) {
+            videoPlayer.fullWindowPlayer
+        } else {
+            videoPlayer
         }
-        //释放所有
-        videoPlayer.setVideoAllCallBack(null)
-        super.onBackPressed()
     }
 }
--- app/src/main/java/com/terry/jetpackmvvm/sample/widget/MarqueeVideoPlayer.java
+++ app/src/main/java/com/terry/jetpackmvvm/sample/widget/MarqueeVideoPlayer.java
@@ -1,28 +1,15 @@
 package com.terry.jetpackmvvm.sample.widget;
 
 import android.content.Context;
-import android.os.Bundle;
-import android.os.Parcelable;
 import android.util.AttributeSet;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer;
-import com.shuyu.gsyvideoplayer.video.base.GSYBaseVideoPlayer;
-import com.superluo.textbannerlibrary.ITextBannerItemClickListener;
 import com.superluo.textbannerlibrary.TextBannerView;
 import com.terry.jetpackmvvm.sample.R;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MarqueeVideoPlayer extends StandardGSYVideoPlayer {
 
     private TextBannerView mTvBanner;
-    private TextView mTvTest;
-    private List<String> mList = new ArrayList<>();
-
-    private IFullscreenListener mListener;
 
     public MarqueeVideoPlayer(Context context, Boolean fullFlag) {
         super(context, fullFlag);
@@ -48,79 +35,10 @@ public class MarqueeVideoPlayer extends StandardGSYVideoPlayer {
     @Override
     protected void init(Context context) {
         super.init(context);
-//        setSaveEnabled(true);
         mTvBanner = findViewById(R.id.tv_banner);
-        mTvTest = findViewById(R.id.tv_test);
-
-//        mList = new ArrayList<>();
-//        mList.add("学好Java、Android、C#、C、ios、html+css+js");
-//        mList.add("走遍天下都不怕！！！！！");
-//        mList.add("不是我吹，就怕你做不到，哈哈");
-//        mList.add("superluo");
-//        mList.add("你是最棒的，奔跑吧孩子！");
-//        /**
-//         * 设置数据，方式一
-//         */
-//        mTvBanner.setDatas(mList);
     }
-
-    @Override
-    public void onPrepared() {
-        super.onPrepared();
-//        onPrepareMarquee(this);
-    }
-
-
-
-    @Override
-    public GSYBaseVideoPlayer startWindowFullscreen(Context context, boolean actionBar, boolean statusBar) {
-        GSYBaseVideoPlayer gsyBaseVideoPlayer = super.startWindowFullscreen(context, actionBar, statusBar);
-//        if (gsyBaseVideoPlayer != null) {
-//            MarqueeVideoPlayer gsyVideoPlayer = (MarqueeVideoPlayer) gsyBaseVideoPlayer;
-//            TextBannerView tvBanner = gsyVideoPlayer.getTextBannerView();
-//            ArrayList<String> list = new ArrayList<>();
-//            list.add("学好Java、Android、C#、C、ios、html+css+js");
-//            list.add("走遍天下都不怕！！！！！");
-//            list.add("不是我吹，就怕你做不到，哈哈");
-//            list.add("superluo");
-//            list.add("你是最棒的，奔跑吧孩子！");
-//            tvBanner.setDatas(list);
-//            tvBanner.startViewAnimator();
-//        }
-        return gsyBaseVideoPlayer;
-    }
-
-//    private void onPrepareMarquee(MarqueeVideoPlayer gsyVideoPlayer) {
-//        if (gsyVideoPlayer.getTextBannerView() != null && !gsyVideoPlayer.getDanmakuView().isPrepared() && gsyVideoPlayer.getParser() != null) {
-//            gsyVideoPlayer.getDanmakuView().prepare(gsyVideoPlayer.getParser(),
-//                    gsyVideoPlayer.getDanmakuContext());
-//        }
-//    }
-
-//    @Override
-//    public Parcelable onSaveInstanceState()
-//    {
-//        Bundle bundle = new Bundle();
-//        bundle.putParcelable("superState", super.onSaveInstanceState());
-//        return bundle;
-//    }
-//
-//    @Override
-//    public void onRestoreInstanceState(Parcelable state)
-//    {
-//        if (state instanceof Bundle) // implicit null check
-//        {
-//        }
-//        super.onRestoreInstanceState(state);
-//    }
 
     public TextBannerView getTextBannerView() {
         return mTvBanner;
-    }
-    public TextView getTvTest() {
-        return mTvTest;
-    }
-    public List<String> getList() {
-        return mList;
     }
 }
