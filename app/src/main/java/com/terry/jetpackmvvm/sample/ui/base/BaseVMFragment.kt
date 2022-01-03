80cc4de1d1e7adfac7fb519b0d26ee664615d68f
Terry <Terry@iter.com.tw>
2022-01-03 16:28:00 +0800

    refactor(app): add BaseVMFragment sample

--- app/src/main/java/com/terry/jetpackmvvm/sample/ui/base/BaseVMFragment.kt
+++ app/src/main/java/com/terry/jetpackmvvm/sample/ui/base/BaseVMFragment.kt
@@ -2,20 +2,26 @@ package com.terry.jetpackmvvm.sample.ui.base
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import java.lang.reflect.ParameterizedType
 
-abstract class BaseVMActivity<VB : ViewDataBinding, VM : ViewModel>(inflateMethod: (LayoutInflater) -> VB) :
-        BaseActivity<VB>(inflateMethod) {
+abstract class BaseVMFragment<VB : ViewDataBinding, VM : ViewModel>(inflateMethod: (LayoutInflater, ViewGroup?, Boolean) -> VB) : BaseFragment<VB>(inflateMethod) {
 
     lateinit var viewModel: VM
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val rootView = super.onCreateView(inflater, container, savedInstanceState)
         viewModel = ViewModelProvider(this).get(getViewModelClass())
+        return rootView
     }
 
     @Suppress("UNCHECKED_CAST")
@@ -24,5 +30,4 @@ abstract class BaseVMActivity<VB : ViewDataBinding, VM : ViewModel>(inflateMetho
             (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[1]
         return type as Class<VM>
     }
-
 }
