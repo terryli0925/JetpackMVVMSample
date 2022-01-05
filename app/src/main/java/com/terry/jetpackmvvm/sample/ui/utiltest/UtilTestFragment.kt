f29adfa87ccddac40cd09f7a06137cac1d8aab2a
Terry <Terry@iter.com.tw>
2022-01-05 14:58:12 +0800

    refactor: remove UtilTestUtils

--- app/src/main/java/com/terry/jetpackmvvm/sample/ui/utiltest/UtilTestFragment.kt
+++ app/src/main/java/com/terry/jetpackmvvm/sample/ui/utiltest/UtilTestFragment.kt
@@ -1,10 +1,19 @@
 package com.terry.jetpackmvvm.sample.ui.utiltest
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
+import com.terry.jetpackmvvm.sample.databinding.DialogLoadingBinding
+import com.terry.jetpackmvvm.sample.databinding.DialogVerificationCodeBinding
 import com.terry.jetpackmvvm.sample.databinding.FragmentUtilTestBinding
 import com.terry.jetpackmvvm.sample.ui.base.BaseFragment
+import com.terry.jetpackmvvm.sample.ui.utiltest.dialog.ScreenshotDialog
 import com.terry.jetpackmvvm.sample.util.RxUtils
 import com.terry.jetpackmvvm.sample.widget.AnimDialog
+import com.terry.jetpackmvvm.sample.widget.VerificationCodeView
 
 class UtilTestFragment : BaseFragment<FragmentUtilTestBinding>(FragmentUtilTestBinding::inflate) {
     private lateinit var viewModel: UtilTestViewModel
@@ -13,24 +22,51 @@ class UtilTestFragment : BaseFragment<FragmentUtilTestBinding>(FragmentUtilTestB
         viewModel = ViewModelProvider(this).get(UtilTestViewModel::class.java)
 
         RxUtils.throttleFirst(viewLifecycleOwner, binding.btnScreenshot, {
-            UtilTestUtils.showScreenShotDialog(parentFragmentManager)
+            showScreenShotDialog(parentFragmentManager)
         })
 
         RxUtils.throttleFirst(viewLifecycleOwner, binding.btnAnimDialogZoom, {
-            UtilTestUtils.showAnimDialog(requireContext(), AnimDialog.AnimInType.ZOOM)
+            showAnimDialog(requireContext(), AnimDialog.AnimInType.ZOOM)
         })
 
         RxUtils.throttleFirst(viewLifecycleOwner, binding.btnAnimDialogTop, {
-            UtilTestUtils.showAnimDialog(requireContext(), AnimDialog.AnimInType.TOP)
+            showAnimDialog(requireContext(), AnimDialog.AnimInType.TOP)
         })
 
         RxUtils.throttleFirst(viewLifecycleOwner, binding.btnAnimDialogBottom, {
-            UtilTestUtils.showAnimDialog(requireContext(), AnimDialog.AnimInType.BOTTOM)
+            showAnimDialog(requireContext(), AnimDialog.AnimInType.BOTTOM)
         })
 
         RxUtils.throttleFirst(viewLifecycleOwner, binding.btnVerificationCodeDialog, {
-            UtilTestUtils.showVerificationViewDialog(requireContext())
+            showVerificationViewDialog(requireContext())
         })
+    }
+
+    private fun showScreenShotDialog(fm: FragmentManager) {
+        ScreenshotDialog().show(fm, "")
+    }
+
+    private fun showAnimDialog(context: Context, type: AnimDialog.AnimInType) {
+        val dialog = AnimDialog(context)
+        val binding = DialogLoadingBinding.inflate(LayoutInflater.from(context))
+        dialog.contentView(binding.root)
+            .animType(type).show()
+    }
+
+    private fun showVerificationViewDialog(context: Context) {
+        val dialog = AnimDialog(context)
+        val binding = DialogVerificationCodeBinding.inflate(LayoutInflater.from(context))
+        binding.verificationCodeView.onCodeFinishListener =
+            object : VerificationCodeView.OnCodeFinishListener {
+                override fun onTextChange(view: View, content: String) {
+                }
+
+                override fun onComplete(view: View, content: String) {
+                    Toast.makeText(context, content, Toast.LENGTH_SHORT).show()
+                    dialog.dismiss()
+                }
+            }
+        dialog.contentView(binding.root).cancelable(false).show()
     }
 
     companion object {
