6c2135642eb6ba6dd54ba92fa695b1b108662e8d
Terry <Terry@iter.com.tw>
2022-02-08 11:20:47 +0800

    feat(demo): add MotionLayout sample

--- app/src/main/java/com/terry/jetpackmvvm/sample/ui/constraintlayoutsample/ConstraintLayoutSampleFragment.kt
+++ app/src/main/java/com/terry/jetpackmvvm/sample/ui/constraintlayoutsample/ConstraintLayoutSampleFragment.kt
@@ -4,12 +4,21 @@ import android.view.View
 import com.terry.jetpackmvvm.sample.databinding.FragmentConstraintLayoutSampleBinding
 import com.terry.jetpackmvvm.sample.ui.base.BaseFragment
 
-class ConstraintLayoutSampleFragment : BaseFragment<FragmentConstraintLayoutSampleBinding>(FragmentConstraintLayoutSampleBinding::inflate) {
+class ConstraintLayoutSampleFragment :
+    BaseFragment<FragmentConstraintLayoutSampleBinding>(FragmentConstraintLayoutSampleBinding::inflate) {
 
     override fun init() {
         binding.tvTitle3.setOnClickListener {
             binding.tvLeft3.visibility =
                 if (binding.tvLeft3.visibility == View.VISIBLE) View.GONE else View.VISIBLE
+        }
+
+        binding.vMlAnchor.setOnClickListener {
+            val ml = binding.ml
+            when(ml.progress) {
+                0f -> ml.transitionToEnd()
+                1f -> ml.transitionToStart()
+            }
         }
     }
 }
